3826bc4613a72c33b6667aedb1e364cc47fbae6b
borg2Borg <56142058+borg2borg@users.noreply.github.com>
2020-07-07 16:08:30 -0400

    Initial commit

---
+++ main.R
@@ -0,0 +1,23 @@
+#? or help
+help("mean")
+
+# Mean
+mean (1:5)
+x = 1
+y = 12
+
+mean(x,y) #mean of x and y, 1 and 12
+
+#series added to another series
+#Variable addition
+1:5 + 6:10 #7 9 11 13 15
+c(1, 3, 6, 10, 15) + c(0, 1, 3, 6, 10) #1 4 9 16 25
+
+#sum 1+2+3+4+5 = 15
+#median number between 1 and 5, 3
+sum(1:5) #15
+median(1:5) #3
+sum(1, 2, 3, 4, 5) #15
+
+#new line
+
